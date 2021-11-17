914186269c37448638bbe16ac77942b905bfa971
Tim <tim.oumpho@gmail.com>
2021-11-17 09:46:41 +0100

    improve accueil & article

--- pages/article.vue
+++ pages/article.vue
@@ -1,15 +1,12 @@
 <template>
   <v-container>
     <v-card>
+      <v-img
+        src="img/banner.jpg"
+        height="250px"
+      ></v-img>
       <v-card-text>
         <v-container>
-          <v-row class="mb-2" justify="center" align="center">
-            <v-avatar size="150px">
-                <v-img
-                  alt="Avatar"
-                  src="img/banner.jpg"
-              /></v-avatar>
-          </v-row>
           <v-row class="mb-2" justify="center">
             <h1>Titre article 1</h1>
           </v-row>
@@ -20,6 +17,7 @@
                 src="img/avatar.jpg"
             /></v-avatar>
             <span class="pl-2">TIーム</span>
+            <span class="pl-2">|</span>
             <span class="pl-2">18/01/2021</span>
           </v-row>
           <nuxt-content :document="page" />
@@ -29,25 +27,46 @@
     <h1>Commentaire</h1>
     <v-card v-for="i in 10" :key="i" class="mt-2">
       <v-card-text>
-        <v-row class="fill-height" align="start">
-          <v-col cols="3">
-            <v-avatar size="120px">
-              <img
-                alt="Avatar"
-                src="img/avatar.jpg"
-            /></v-avatar>
-            <span class="pl-2">Utilisateur {{ i }}</span>
-          </v-col>
+        <v-row>
           <v-col>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Asperiores quo sapiente, quis sequi, blanditiis fugiat delectus
-              atque ab ducimus magnam voluptatum rem inventore dolorem? Maiores
-              perspiciatis consequatur deserunt pariatur dignissimos.
-            </p>
+            <v-row class="mb-4 fill-height text-center" no-gutters>
+              <v-col cols="12">
+                <v-avatar size="120px">
+                  <v-img
+                    alt="Avatar"
+                    src="img/banner.jpg"
+                  />
+                </v-avatar>
+              </v-col>
+              <v-col cols="12">
+                <span class="pl-2">TIーム</span>
+              </v-col>
+              <v-col cols="12">
+                <v-row class="ml-1" justify="center" align="center">
+                  <v-chip-group>
+                    <v-chip color="blue" x-small> Développeur Web </v-chip>
+                  </v-chip-group>
+                </v-row>
+              </v-col>
+            </v-row>
+          </v-col>
+          <v-col cols="9">
+            <v-row>
+              <span class="font-weight-black">Editer | 00/00/00</span>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Asperiores quo sapiente, quis sequi, blanditiis fugiat delectus
+                atque ab ducimus magnam voluptatum rem inventore dolorem? Maiores
+                perspiciatis consequatur deserunt pariatur dignissimos.
+              </p>
+            </v-row>
           </v-col>
         </v-row>
       </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <span>Info ramdom jsp</span>
+      </v-card-actions>
     </v-card>
     <v-card class="mt-2">
       <v-card-text>
--- pages/index.vue
+++ pages/index.vue
@@ -15,21 +15,22 @@
             :src="item.src"
             nuxt
             to="/article"
-            ><v-row
+            >
+            <v-row
               class="fill-height pl-7 font-weight-black"
               align="end"
               justify="start"
             >
               <div class="title">Article numéro {{ i }}</div>
-            </v-row></v-carousel-item
-          >
+            </v-row>  
+          </v-carousel-item>
         </v-carousel>
       </v-col>
     </v-row>
     <v-row>
-      <v-col cols="12" xl="8" lg="8" md="8" sm="8">
+      <v-col cols="12" xl="8" lg="8" md="12" sm="12">
         <v-card
-          v-for="i in articleNumber"
+          v-for="i in 2"
           :key="i"
           class="card_news"
           elevation="0"
@@ -70,8 +71,51 @@
           </v-card-text>
           <v-divider class="pb-6"></v-divider>
         </v-card>
+        <v-card
+          v-for="i in articleNumber"
+          :key="i"
+          class="card_news"
+          elevation="0"
+          to="/article"
+        >
+          <v-card-text>
+            <v-row>
+              <v-col v-if="!$vuetify.breakpoint.mobile" class="text-center align-self-center" cols="2">
+                <v-avatar size="100px" tile>
+                  <v-img class="rounded-lg" alt="Avatar" src="img/banner.jpg"
+                /></v-avatar>
+              </v-col>
+              <v-col :cols="$vuetify.breakpoint.mobile ? 12 : 10" class="d-flex flex-column">
+                <v-row align="center">
+                  <h2>Article {{ i }}</h2>
+                  <v-spacer></v-spacer>
+                  <v-chip-group>
+                    <v-chip x-small> tag 1 </v-chip>
+                    <v-chip x-small> bit </v-chip>
+                  </v-chip-group>
+                </v-row>
+                <v-row class="fill-height" align="center"
+                  ><v-avatar size="30px">
+                    <img alt="Avatar" src="img/avatar.jpg"
+                  /></v-avatar>
+                  <span class="pl-2">Utilisateur</span>
+                  <span class="pl-2">|</span>
+                  <span class="pl-2">00/00/0000</span>
+                </v-row>
+                <v-row>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Blanditiis neque facilis vel voluptatem molestiae dolorum sit
+                    aperiam molestias ullam minima minus illo tenetur eum
+                    consectetur sapiente maxime, praesentium quia quisquam?
+                  </p>
+                </v-row>
+              </v-col>
+            </v-row>
+          </v-card-text>
+        </v-card>
       </v-col>
-      <v-col>
+      <v-col v-if="!$vuetify.breakpoint.mobile">
         <v-card width="100%">
           <v-card-title
             >Recommandé <v-spacer></v-spacer
